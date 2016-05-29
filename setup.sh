c630e94d11a4d5c352a9098a74d29fec704eff8f
Takuo WATANABE <takuo@acm.org>
2016-05-29 11:29:13 +0900

    Setup script merges with existing directories

--- setup.sh
+++ setup.sh
@@ -8,6 +8,13 @@ for f in $thisdir/dot.*; do
     f0=$(basename $f)
     f1=$HOME/${f0##dot}
     if [ $f1 != $HOME/ ]; then
+        if [ -d $f -a -d $f1 -a ! -L $f1 ]; then
+            for g in $f1/*; do
+                if [ ! -e $f/$(basename $g) ]; then
+                    mv $g $f
+                fi
+            done
+        fi
         rm -rf $f1
         ln -sf $f $f1
     fi
